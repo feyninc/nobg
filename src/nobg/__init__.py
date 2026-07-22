12967c848c0e8f572affc1595750c91ecfd7df49
Hafedh Hichri <lain@chonkie.ai>
2026-07-22 19:12:28 +0100

    Update __init__.py

--- src/nobg/__init__.py
+++ src/nobg/__init__.py
@@ -4,4 +4,4 @@ from .birefnet.modeling_birefnet import BiRefNet
 from .birefnet.image_processing_birefnet import BiRefNetImageProcessor
 from .auto import AutoModel, AutoProcessor
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
